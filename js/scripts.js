c0e24aecac2854b018a20d1d69bd35ff2c5636c8
Lew Davidson <lewdavidson47@gmail.com>
2017-10-20 12:14:09 -0700

    add else function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  $("form#which-lang").submit(function(event){
+  $("form#which-lang").submit(function(event) {
     var name = $("select#name").val();
     var end = $("select#frnt-or-bk").val();
     var strokes = $("select#strokes").val();
@@ -8,63 +8,20 @@ $(document).ready(function(){
     var device = $("select#device").val();
     var important = $("input:radio[name=important]:checked").val();
 
-      if (important === "looks" && end === "front" && strokes === "broad" && platform === "app" && device === "ios") {
+
+    if (end === "front" && strokes === "broad" && platform === "app" && device === "ios" && important === "looks") {
         $("#design").show();
-  }
-      else if (important === "diverse" && end === "back" && strokes === "broad" && platform === "web" || platform === "app" && device === "android") {
+    } else if (end === "back" && strokes === "broad" && platform === "web" || platform === "app" && device === "android" && important === "flex") {
         $("#java").show();
-    }
-      else if (important === "function" && end === "back" && strokes === "narrow" && platform === "web" && device === "ios" || device === "android") {
+    } else if (end === "back" && strokes === "narrow" && platform === "web" && device === "ios" || device === "android" && important === "function") {
         $("#ruby").show();
-    }
-     else {
-       $("#confilict").show();
-     }
+    } else {
+     $("#confilict").show();
+   }
+
+
 
     event.preventDefault();
   });
 
 });
-/*
-var css = 0;
-var java = 0;
-var ruby = 0;
-
-if (end === "front") {
-      css++;
-    }
-if (strokes === "broad") {
-    css++;
-}
-if (platform === "web" || platform === "app") {
-    css++;
-}
-if (device === "ios") {
-    css++;
-}
-if (important === "design") {
-    css++;
-}
-if (end === "back") {
-      java++;
-    }
-if (strokes === "broad") {
-      java++;
-}
-if (platform === "web" || platform === "app") {
-      java++;
-}
-if (device === "android") {
-      java++;
-}
-if (important === "diverse") {
-      java++;
-}
-
-
-    if (css >= 3) {
-      $("#design").show();
-        }
-    if (java >= 3) {
-      ("#java").show();
-    }*/
