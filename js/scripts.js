0795dd69b66db30af583ac56da4ea2944428d29a
Lew Davidson <lewdavidson47@gmail.com>
2017-10-20 09:09:24 -0700

    add submit function

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+  $("form#which-lang").submit(function(event){
+    var name = $("select#name").val();
+    var end = $("select#frnt-or-bk").val();
+    var strokes = $("select#strokes").val();
+    var platform = $("select#platform").val();
+    var device = $("select#device").val();
+    var important = $("input:radio[name=important]:checked").val();
+
+
+    event.preventDefault();
+  });
+
+
+
+
+
+
+
+
+});
